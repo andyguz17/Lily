5d40e2ffce80b7f258f701e525549aa54fd4083d
andyguz17 <andyguz17@gmail.com>
2022-03-06 12:55:39 -0400

    Enable storybook

---
+++ src/stories/Button.stories.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import Button from '../components/Button';
+
+export default {
+  title: 'Lily/Button',
+  component: Button,
+} as ComponentMeta<typeof Button>;
+
+const Template: ComponentStory<typeof Button> = args => <Button {...args} />;
+
+export const HelloWorld = Template.bind({});
+HelloWorld.args = {
+  children: 'Hello World',
+};
+
+export const ClickMe = Template.bind({});
+ClickMe.args = {
+  children: 'Click Me',
+};
--- src/tests/Button.test.tsx
+++ src/tests/Button.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 
-import Button from './Button';
+import Button from '../components/Button';
 
 describe('Button', () => {
   test('Render the Button Component', () => {
