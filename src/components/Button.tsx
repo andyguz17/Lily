a855589c63964cd880352bc617e4316611ddde5f
andyguz17 <andyguz17@gmail.com>
2022-03-06 16:25:53 -0400

    Test pipeline

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,11 +2,23 @@ import React from 'react';
 import '../styles/Button.scss';
 
 export interface IButton {
+  color?: IButtonColor;
   children?: React.ReactNode;
+  onClick?: () => void;
 }
 
-const Button: React.FC = ({ children }: IButton): JSX.Element => {
-  return <button>{children}</button>;
+export type IButtonColor = 'primary' | 'secondary';
+
+const Button: React.FC = ({
+  children,
+  color,
+  onClick,
+}: IButton): JSX.Element => {
+  return (
+    <button onClick={onClick} className={`btn btn-${color}`}>
+      {children}
+    </button>
+  );
 };
 
 export default Button;
