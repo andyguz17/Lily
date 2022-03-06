0a3abdb2067aebca29a995e0b0f9bec5ece49656
andyguz17 <andyguz17@gmail.com>
2022-03-06 12:58:48 -0400

    Enable scss

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,13 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/preset-scss',
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "webpack5"
-  }
-}
+  framework: '@storybook/react',
+  core: {
+    builder: 'webpack5',
+  },
+};
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../styles/Button.css';
+import '../styles/Button.scss';
 
 export interface IButton {
   children?: React.ReactNode;
